c8f5879ffb575c5e22682b15dcff7911a8781984
Parth Talwar <parthtalwar@hotmail.com>
2023-05-09 16:09:50 +1200

    Converted python code to c# (still needs to be converted to GUI)

--- Program.cs
+++ Program.cs
@@ -1,51 +1,55 @@
-﻿import random
-import time
-import pickle
+﻿
+Console.WriteLine("Welcome to The Adventures of ___");
+Console.WriteLine("1. Start Game");
+Console.WriteLine("2. Options");
+Console.WriteLine("3. Quit");
+Console.WriteLine("Select your option: ");
 
-def start():
+string menuSelect = Console.ReadLine();
 
-    print("Welcome to The Adventures of ___")
+//If the player selects 1 to start the game
+if (menuSelect == "1")
+{
+    Console.WriteLine("...");
+    Console.WriteLine("...");
+    Console.WriteLine("*Inaudable noise*");
+    Console.WriteLine("Ugh, what happened?");
+    Console.WriteLine("Where am I?");
+    Console.WriteLine("*Stands up*");
+    Console.WriteLine("I gotta ask someone what this place is");
+    Console.WriteLine("Last time I remember, I was out in the fields before a beast appeared out of the ground");
+    Console.WriteLine("...");
+    Console.WriteLine("Why are some people giving me a strange glance...");
+    Console.WriteLine("I gotta ask someone about this place");
+    Console.WriteLine("*Approches person*");
+    Console.WriteLine("Oh, hello! What can I help you with?\n1. Inquire about the place\n2. Pickpocket ");
+    string selection1 = Console.ReadLine();
 
-    print("1. Start Game")
+    if (selection1 == "1")
+    {
+        Console.WriteLine("Oh, you're a newcomer huh? Well, this is Saber village.\n");
+    }
 
-    print("2. Options")
+    else if (selection1 == "2")
+    {
+        Console.WriteLine("Hey! What are you doing?! GET BACK HERE!");
+        Console.WriteLine("*After reaching a safe space within the village...*");
+        Console.WriteLine("Right, looks like I'm away from him");
+        Console.WriteLine("Now that I have some money, I guess I should get some food.\nI'm starving...");
+    }
 
-    print("3. Quit")
+}
 
-menu = input("Select your option: ")
 
-#If the player selects 1 to start the game
-if menu == "1":
-    print("...")
-    print("...")
-    print("*Inaudable noise*")
-    print("Ugh, what happened?")
-    print("Where am I?")
-    print("*Stands up*")
-    print("I gotta ask someone what this place is")
-    print("Last time I remember, I was out in the fields before a beast appeared out of the ground")
-    print("...")
-    print("Why are some people giving me a strange glance...")
-    print("I gotta ask someone about this place")
-    print("*Approches person*")
+//If the player selects 2 for settings
+else if (menuSelect == "2")
+{
+    Console.WriteLine("Settings:");
+}
 
-    selection1 = input("Oh, hello! What can I help you with?\n1. Inquire about the place\n2. Pickpocket ")
-
-    if selection1 == "1":
-        print("Oh, you're a newcomer huh? Well, this is Saber village.\n")
-    elif selection1 == "2":
-        print("Hey! What are you doing?! GET BACK HERE!")
-        print("*After reaching a safe space within the village...*")
-        print("Right, looks like I'm away from him")
-        print("Now that I have some money, I guess I should get some food.\nI'm starving...")
-        print("After having a meal from a local restaurant")
-        selection2b = input("")
-
-#If the player selects 2 for settings
-elif menu == "2":
-    print("Settings:")
-
-#If the player selects 3 to quit the game
-elif menu == "3":
-    print("Thank you for visiting ___!")
-    quit()
+//If the player selects 3 to quit the game
+else if (menuSelect == "3")
+{
+    Console.WriteLine("Thank you for visiting ___!");
+}
+   
