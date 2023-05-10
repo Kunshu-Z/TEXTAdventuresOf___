7c59641613833e26549b1820741c890b463f3049
Jordan Vera <Jordan.Vera01@student.weltec.ac.nz>
2023-05-10 14:27:57 +1200

    addded wait times and intro

--- Program.cs
+++ Program.cs
@@ -1,19 +1,35 @@
-﻿Console.WriteLine("...");
+﻿using System.Threading;
+
+Console.WriteLine("...");
+Thread.Sleep(1000);
 Console.WriteLine("'Light'");
+Thread.Sleep(1000);
 Console.WriteLine("...");
+Thread.Sleep(1000);
 Console.WriteLine("''I see light''");
+Thread.Sleep(1000);
 Console.WriteLine("...");
+Thread.Sleep(1000);
 Console.WriteLine("What's my name?");
+Thread.Sleep(1000);
 Console.WriteLine("Who am I?");
+Thread.Sleep(1000);
 Console.WriteLine("*A sweeping breeze hits the back of your head*");
+Thread.Sleep(1000);
 Console.WriteLine("*You come to a revelation*");
+Thread.Sleep(1000);
 Console.WriteLine("My name... It's - ");
 string myName = Console.ReadLine();
 Console.WriteLine($"My name... It's {myName} ");
+Thread.Sleep(1000);
 Console.WriteLine("Suddenly a loud roar is heard and you are transported to grassy plains");
+Thread.Sleep(1000);
 Console.WriteLine("And then...");
+Thread.Sleep(1000);
 Console.WriteLine("...");
+Thread.Sleep(1000);
 Console.WriteLine("...");
+Thread.Sleep(1000);
 Console.WriteLine($"Welcome to The Adventures of {myName}");
 Console.WriteLine("1. Start Game");
 Console.WriteLine("2. Options");
@@ -26,17 +42,29 @@ string menuSelect = Console.ReadLine();
 if (menuSelect == "1")
 {
     Console.WriteLine("...");
+    Thread.Sleep(1000);
     Console.WriteLine("...");
+    Thread.Sleep(1000);
     Console.WriteLine("*Inaudable noise*");
+    Thread.Sleep(1000);
     Console.WriteLine("Ugh, what happened?");
+    Thread.Sleep(1000);
     Console.WriteLine("Where am I?");
+    Thread.Sleep(1000);
     Console.WriteLine("*Stands up*");
+    Thread.Sleep(1000);
     Console.WriteLine("I gotta ask someone what this place is");
+    Thread.Sleep(1000);
     Console.WriteLine("Last time I remember, I was out in the fields before a beast appeared out of the ground");
+    Thread.Sleep(1000);
     Console.WriteLine("...");
+    Thread.Sleep(1000);
     Console.WriteLine("Why are some people giving me a strange glance...");
+    Thread.Sleep(1000);
     Console.WriteLine("I gotta ask someone about this place");
+    Thread.Sleep(1000);
     Console.WriteLine("*Approches person*");
+    Thread.Sleep(1000);
     Console.WriteLine("Oh, hello! What can I help you with?\n1. Inquire about the place\n2. Pickpocket ");
     string selection1 = Console.ReadLine();
 
@@ -48,9 +76,13 @@ if (menuSelect == "1")
     else if (selection1 == "2")
     {
         Console.WriteLine("Hey! What are you doing?! GET BACK HERE!");
+        Thread.Sleep(1000);
         Console.WriteLine("*After reaching a safe space within the village...*");
+        Thread.Sleep(1000);
         Console.WriteLine("Right, looks like I'm away from him");
+        Thread.Sleep(1000);
         Console.WriteLine("Now that I have some money, I guess I should get some food.\nI'm starving...");
+        Thread.Sleep(1000);
     }
 
 }
